8809a35abb327754d67ec3df05738de88a42e350
Andrew Sears <andrew.sears@t4g.com>
2017-02-12 09:15:52 -0500

    gitatme-update_deployment_script

--- deploy.cmd
+++ deploy.cmd
@@ -69,7 +69,7 @@ echo Handling .NET Web Application deployment.
 
 :: 1. Restore NuGet packages
 IF /I "mcbot.sln" NEQ "" (
-  call :ExecuteCmd nuget restore "%DEPLOYMENT_SOURCE%\mcbot.sln"
+  call :ExecuteCmd nuget restore "%DEPLOYMENT_SOURCE%\Bot.sln"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
